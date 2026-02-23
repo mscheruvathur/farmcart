41b0dca931e5f2caa15b373147a8113c24e71519
mscheruvathur <mscheruvathur@gmail.com>
2026-02-23 16:02:10 +0530

    Fix layout heights in OrangeBannerSection and add empty media query for responsiveness

--- src/components/OrangeBannerSection.tsx
+++ src/components/OrangeBannerSection.tsx
@@ -3,7 +3,7 @@ import orangeImage from '../assets/orange-bg.jpg';
 export function OrangeBannerSection() {
   return (
     <section className="w-full">
-      <div className="h-[280px] w-full sm:h-[380px] md:h-[650px] lg:h-[720px] xl:h-[780px]">
+      <div className="h-[280px] w-full sm:h-[380px] md:h-[50px] lg:h-[20px] xl:h-[680px]">
         <img
           src={orangeImage}
           alt="Fresh oranges"
